5604ac3c8a90a6d8d600e3aab53626ceda3c6e0e
Voloclkzx <kartyshovvladimir@gmail.com>
2025-11-19 17:40:55 +0300

    Начальная структура проекта

---
+++ src/main/java/model/Task.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Task {
+    public String name;
+    public String description = null;
+    public Date date;
+    public boolean isComplete;
+
+}
---
+++ src/main/java/service/TaskManager.java
@@ -0,0 +1,17 @@
+package service;
+
+public class TaskManager {
+
+    public void addTask() {
+
+    }
+    public void removeTask() {
+
+    }
+    public void changeTask() {
+
+    }
+    public void completeTask() {
+        
+    }
+}
